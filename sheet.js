8a8717693918f363f8e4855c62d17c218a853312
Novatruepower <patatotrape3@gmail.com>
2025-07-08 13:01:40 -0400

    Update sheet.js

--- sheet.js
+++ sheet.js
@@ -245,6 +245,8 @@ function calculateTotal(statName) {
 
 // Helper function to get the applied racial change for a stat (for both Demi-humans and Mutants)
 function getAppliedRacialChange(charData, statName) {
+
+    console.log(charData);
     let totalRacialChange = ExternalDataManager.getRacialChange(charData.race, statName);
 
     if (charData.race === 'Demi-humans') {
