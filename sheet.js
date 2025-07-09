c52fd5dc3a317d1b49a454924792f6ff540c8b5f
Novatruepower <patatotrape3@gmail.com>
2025-07-09 14:24:47 -0400

    debug

--- sheet.js
+++ sheet.js
@@ -52,6 +52,7 @@ function adjustValue(oldMaxValue, value, newMaxValue) {
 
 // Recalculate derived properties
 function recalculateUpdate(char) {
+    console.log(char);
     let oldMaxValue = char.maxHealth;
     char.maxHealth = calculateMaxHealth(char, char.race, char.level, char.healthBonus);
     char.Health.value = adjustValue(oldMaxValue, char.Health.value, char.maxHealth);
