690c71c876f105e23fd0ea78d429d29a5344f4fd
Novatruepower <patatotrape3@gmail.com>
2025-07-12 03:53:38 -0400

    debug

--- ExternalDataManager.js
+++ ExternalDataManager.js
@@ -111,7 +111,7 @@ export const ExternalDataManager = {
                 // Iterate through each category for the character (e.g., 'passives', 'skills')
                 for (const [categoryKey, categoryData] of Object.entries(characterData)) {
                     // Get the array of abilities, which is a collection of values
-                    const abilities = manualPassivesData[characterKey][categoryKey].manualPassives;
+                    const abilities = categoryData[categoryKey].manualPassives;
                     console.log(abilities);
                     this._data[characterKey] = this._data[characterKey] || {};
                     this._data[characterKey][categoryKey] = this._data[characterKey][categoryKey] || {};
