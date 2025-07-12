b3d1d3c9e54bc2ad54ce714a41b425f5a18e877a
Novatruepower <patatotrape3@gmail.com>
2025-07-12 11:17:19 -0400

    debug

--- ExternalDataManager.js
+++ ExternalDataManager.js
@@ -120,7 +120,8 @@ export const ExternalDataManager = {
                     for (const [abilityKey, abilityData] of Object.entries(passives.manualPassives)) {
                         for (const [optionKey, optionData] of Object.entries(abilityData.options)) {
                             const option = this._data[characterKey][categoryKey].manualPassives[abilityKey][optionKey];
-                            console.log(this._data[characterKey][categoryKey].manualPassives);
+                            console.log(this._data[characterKey][categoryKey].manualPassives[abilityKey]);
+                            console.log(abilityKey);
 
                             if (option.applicableStats)
                                 option.applicableStats = this.replaceDataStats(option.applicableStats);
