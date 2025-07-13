167c7807a2a5a49751aac73a2d91847724d16b22
Novatruepower <patatotrape3@gmail.com>
2025-07-12 22:44:37 -0400

    test

--- ExternalDataManager.js
+++ ExternalDataManager.js
@@ -101,8 +101,7 @@ export const ExternalDataManager = {
             for (const [characterKey, characterData] of Object.entries(manualPassivesData)) {
                 const characterTarget = this._data[characterKey] ||= {};
                 for (const [categoryKey, categoryData] of Object.entries(characterData)) {
-                    console.log(Object.keys(categoryData));
-                    characterTarget[categoryKey][categoryData] = {};
+                    characterTarget[categoryKey]['manualPassives'] = categoryData.manualPassives;
 
                     const abilities = categoryData.manualPassives || {};
                     for (const abilityData of Object.values(abilities)) {
