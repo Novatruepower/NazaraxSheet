4e07d6dd3e56c15260f583db2a13169a888a154c
Novatruepower <patatotrape3@gmail.com>
2025-08-05 02:19:11 -0400

    fix?

--- ExternalDataManager.js
+++ ExternalDataManager.js
@@ -255,7 +255,7 @@ export const ExternalDataManager = {
                             for (let i = 0; i < option.options.values.length; i++) {
                                 const newOption = { ...template };
                                 newOption.value = option.options.values[i];
-                                newOption.label = option.label.replace('%', `${newOption.value*100}%`);
+                                newOption.label = option.label.replace('%', `${Math.abs(newOption.value*100)}%`);
                                 newOption.count = option.options.counts[i];
                                 expandedOptions.push(newOption);
                             }
