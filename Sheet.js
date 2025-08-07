5f55236e8ea6ba08ebc8024ea4f7f2ad74659232
Novatruepower <patatotrape3@gmail.com>
2025-08-07 16:40:18 -0400

    test hasUnsavedChanges

--- Sheet.js
+++ Sheet.js
@@ -2489,9 +2489,8 @@ function applyHistoryState(state) {
     }
     updateDOM();
     populateCharacterSelector(); // Update selector in case character names changed
-    hasUnsavedChanges = false; // Reverted/Forwarded state is now considered "saved" locally
+    hasUnsavedChanges = historyPointer != historyStack.length - 1;
     updateHistoryButtonsState(); // Update button states after applying history
-    //character.isDistributingStats = false; // Exit distribution mode on history change
     updateRemainingPointsDisplay(); // Reset remaining points display
 }
 
