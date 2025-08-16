b24cecbf9ea7edac36cb2a484c10ed3fce9f184a
Novatruepower <patatotrape3@gmail.com>
2025-08-15 22:16:01 -0400

    debug

--- Sheet.js
+++ Sheet.js
@@ -1171,6 +1171,7 @@ function processRacialFullAutoPassiveChange(category, newAbilityData) {
     if (character.uniqueIdentifiers['Spatial Reserve'] && newAbilityData.identifier == 'Spatial Reserve') {
         character.BaseRacialPower.value += 90;
         delete character.uniqueIdentifiers[newAbilityData.identifier];
+        console.log(character.BaseRacialPower.value);
     }
     removeTemporaryEffectByIdentifier(newAbilityData, category);
 
