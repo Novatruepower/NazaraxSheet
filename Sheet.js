52ba12c147353d692d19732e36f35a03bcbb7236
Novatruepower <patatotrape3@gmail.com>
2025-08-24 15:04:42 -0400

    fix?

--- Sheet.js
+++ Sheet.js
@@ -2621,7 +2621,7 @@ function updateSpecializationDropdownAndData() {
                    class="form-checkbox h-4 w-4 text-indigo-600 dark:text-indigo-400 rounded border-gray-300 dark:border-gray-600 focus:ring-indigo-500"
                    ${character.specializations[classe] && character.specializations[classe].includes(specName) ? 'checked' : ''}
                />
-               <label for="specializations-${classe}-${specName}" class="ml-2 text-sm text-gray-700 dark:text-gray-300 cursor-pointer">${selectedClass}→${specName}</label>
+               <label for="specializations-${classe}-${specName}" class="ml-2 text-sm text-gray-700 dark:text-gray-300 cursor-pointer">${classe}→${specName}</label>
            `;
             specializationDropdownOptions.appendChild(checkboxDiv);
             });
