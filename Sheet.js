f58ebfec7216a7418d4b41306beabfd15ed0eb9e
Novatruepower <patatotrape3@gmail.com>
2025-08-23 23:51:16 -0400

    test

--- Sheet.js
+++ Sheet.js
@@ -290,7 +290,7 @@ function recalculateCharacterDerivedProperties(char, isSmallDisplay = false) {
         }
     });
 
-    if (character.race)
+    if (character && character.race)
         renderRacial();
 }
 
