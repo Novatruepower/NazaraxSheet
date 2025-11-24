c4351581ca1ee91bb251bf68e2f16f2a5cb37160
Novatruepower <patatotrape3@gmail.com>
2025-11-24 01:11:42 -0500

    test

--- Sheet.js
+++ Sheet.js
@@ -1,5 +1,5 @@
 import { ExternalDataManager } from './scripts/ExternalDataManager.js';
-import { evaluate } from './mathjs.js';
+import { evaluate } from './mathjs';
 let currentGoogleDriveFileId = null; // To store the ID of the currently loaded Google Drive file
 
 const defaultStatMaxExperience = 7;
